4d2cc7e49702ffaa7e02394b36ae1ae68aa690e4
HondaCB650R <prmand-4@student.ltu.se>
2022-05-12 17:53:29 +0200

    update

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,7 +9,7 @@ class CalculatorTest {
     @DisplayName("2 + 1 = 3")
     void addsTwoNumbers() {
         Calculator calculator = new Calculator();
-        assertEquals(3, calculator.add(2, 1), "2 + 1 should equal 3");
+        assertEquals(3, calculator.add(2,1),"2 + 1 should equal 3");
 
     }
 
@@ -20,5 +20,22 @@ class CalculatorTest {
         assertEquals(1, calculator.subtract(2, 1), "2 - 1 should equal 1");
 
     }
+
+    @Test
+    @DisplayName("5 * 5 = 25")
+    void multiplyTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(25, calculator.multiply(5,5),"5 * 5 should equal 25");
+    }
+
+    @Test
+    @DisplayName("30 / 2 = 15")
+    void divideTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(15, calculator.divide(30, 2), "30 / 2 should equal 15");
+
+    }
+
+
 
 }
